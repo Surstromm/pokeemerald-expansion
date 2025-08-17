f63080e30c28d082fa41c428587e850286f0ca12
Surstromm <dennisplug@gmail.com>
2025-08-17 17:22:21 +0200

    Zinnia ow fixed.

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -4921,7 +4921,7 @@ $(MISCGFXDIR)/emotes.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 2
 
 $(OBJEVENTGFXDIR)/people/zinnia.4bpp: %.4bpp: %.png
-	$(GFX) $< $@ -mwidth 4 -mheight 4
+	$(GFX) $< $@ -mwidth 2 -mheight 4
 
 # All pokeballs are 16x32
 $(OBJEVENTGFXDIR)/misc/ball_%.4bpp: $(OBJEVENTGFXDIR)/misc/ball_%.png ; $(GFX) $< $@ -mwidth 2 -mheight 4
